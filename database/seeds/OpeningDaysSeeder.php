326b436e64ec5eea4a3e474b29cd89ed1445b415
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-04-11 20:30:00 +0200

    Days seeder added

---
+++ database/seeds/OpeningDaysSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\OpeningDay;
+
+class OpeningDaysSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('opening_days')->delete();
+        $days = File::get('database/data/days.json');
+        $dayList = json_decode($days);
+        foreach($dayList->days as $city) {
+            $newDay = OpeningDay::create([
+                'name' => $city->name . ' '
+            ]);
+            $newDay->save();
+        }
+    }
+}
