414406c72eaf38504c3a2cdae6b7c4cda60750cb
Alessandro Capurso <a.capurso23@studenti.uniba.it>
2020-04-17 12:45:56 +0200

    added table for proposal

---
+++ database/migrations/2020_04_17_121121_create_table_proposals.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProposals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('rider_id');
+            $table->unsignedBigInteger('restaurateur_id');
+            $table->unsignedBigInteger('order_id');
+            $table->timestamps();
+
+            $table->foreign('rider_id')->references('id')->on('riders')->onDelete('restrict');
+            $table->foreign('restaurateur_id')->references('id')->on('restaurateurs')->onDelete('restrict');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('restrict');
+
+            $table->unique(['rider_id', 'restaurateur_id', 'order_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposals');
+    }
+}
