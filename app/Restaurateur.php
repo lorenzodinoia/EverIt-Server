919ad6924b36617090a667c7e930ad86d4d84111
Lorenzo Di Noia <lorenzodn.98@gmail.com>
2020-08-16 18:20:15 +0200

    Fixed restaurateur review's average when there's no reviews

--- app/Restaurateur.php
+++ app/Restaurateur.php
@@ -109,7 +109,7 @@ class Restaurateur extends Authenticatable
 
     public function getAvgAttribute(){
         $reviews = $this->reviews()->get();
-        if(isset($reviews)){
+        if((isset($reviews)) && (sizeof($reviews) > 0)){
             $count = 0.0;
             $n = sizeof($reviews);
             foreach($reviews as $review){
