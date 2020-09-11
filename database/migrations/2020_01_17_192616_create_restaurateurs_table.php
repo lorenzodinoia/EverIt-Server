ce1df8242aefd4ff4481cbf9ed770299d3d792de
Alessandro <a.capurso23@studenti.uniba.it>
2020-09-11 20:55:42 +0200

    changed default value delivery_cost from 0 to 1

--- database/migrations/2020_01_17_192616_create_restaurateurs_table.php
+++ database/migrations/2020_01_17_192616_create_restaurateurs_table.php
@@ -24,7 +24,7 @@ class CreateRestaurateursTable extends Migration
             $table->string('password');
             $table->string('vat_number', 11);
             $table->integer('max_delivery_time_slot')->unsigned();
-            $table->float('delivery_cost', 4, 2)->default(0);
+            $table->float('delivery_cost', 4, 2)->default(1);
             $table->integer('min_price')->default(1)->unsigned();
             $table->string('image_path')->nullable();
             $table->unsignedBigInteger('shop_type_id');
